cf3ace3f541594481f0baa5fa11e2eb78368d288
zjy <zjy@DESKTOP-6FSSMCF>
2016-06-15 13:30:35 +0800

    熊猫易保

---
+++ src/main/java/com/sivalabs/check/ParamsCheck.java
@@ -0,0 +1,17 @@
+package com.sivalabs.check;
+
+import com.sivalabs.entity.request.InsuredPersonBean;
+
+/**
+ * 接口输入参数校验
+ * @author dragonwu
+ *
+ */
+public class ParamsCheck {
+	 public static boolean checkCompany(){
+		 return true;
+	 }
+	 public static boolean checkInsuredPerson(InsuredPersonBean person){
+		 return true;
+	 }
+}
